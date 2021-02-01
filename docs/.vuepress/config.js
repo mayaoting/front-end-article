3229902993a69128111129bc8ab1caa6c96da58d
maqiqi <2510147466@qq.com>
2021-02-01 15:28:49 +0800

    init blog

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+/*
+ * @Author: your name
+ * @Date: 2021-02-01 14:26:06
+ * @LastEditTime: 2021-02-01 15:21:59
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \mini-componentsd:\git-project\front-end-article\docs\.vuepress\config.js
+ */
+
+module.exports = {
+  base: '/front-end-article/',
+  title: 'front-end-article',
+  themeConfig: {
+    sidebar: [
+      '/',
+      ['/01', '1.introduce'],
+      ['/02', '2.union standard']
+    ],
+    smoothScroll: true,
+  }
+}
