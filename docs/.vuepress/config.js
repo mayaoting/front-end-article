7a440125cf36e819a99313f853352195e07ddac7
maqiqi <2510147466@qq.com>
2021-02-01 18:01:58 +0800

    modify config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-02-01 14:26:06
- * @LastEditTime: 2021-02-01 15:21:59
+ * @LastEditTime: 2021-02-01 18:01:01
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \mini-componentsd:\git-project\front-end-article\docs\.vuepress\config.js
@@ -9,10 +9,10 @@
 
 module.exports = {
   base: '/front-end-article/',
-  title: 'front-end-article',
+  title: 'front-end-articles',
   themeConfig: {
     sidebar: [
-      '/',
+      ['/', '简介']
       ['/01', '1.introduce'],
       ['/02', '2.union standard']
     ],
