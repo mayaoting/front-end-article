45f1403d35ef07b2abc2d2bf3275581fc00273fe
maqiqi <2510149466@qq.com>
2021-02-03 15:27:58 +0800

    add webpack plugin and loader

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-02-01 14:26:06
- * @LastEditTime: 2021-02-01 18:07:00
+ * @LastEditTime: 2021-02-03 14:23:33
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \mini-componentsd:\git-project\front-end-article\docs\.vuepress\config.js
@@ -14,7 +14,8 @@ module.exports = {
     sidebar: [
       ['/', '简介'],
       ['/01', '1.introduce'],
-      ['/02', '2.union standard']
+      ['/02', '2.union standard'],
+      ['/03', '3.webpack 学习点']
     ],
     smoothScroll: true,
   }
